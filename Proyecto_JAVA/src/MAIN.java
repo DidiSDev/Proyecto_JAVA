e970c7da2be139d67b4f0e7a8b71a90243d087d7
jasvi23 <juanan2395@gmail.com>
2024-11-19 18:41:54 +0100

    cambios en venGesCit

--- Proyecto_JAVA/src/MAIN.java
+++ Proyecto_JAVA/src/MAIN.java
@@ -4,10 +4,12 @@ public class MAIN {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		BaseDatos bd = new BaseDatos();
-		ventanaLogin ventanaLogin=new ventanaLogin(bd);
-		ventanaLogin.setVisible(true);
+		/*ventanaLogin ventanaLogin=new ventanaLogin(bd);
+		ventanaLogin.setVisible(true);*/
 		//ventanaCita vc = new ventanaCita();
-
+		
+		ventanaGestionCitas vgc = new ventanaGestionCitas();
+		vgc.setBounds(10,10,800,800);
 	}
 
 }
--- Proyecto_JAVA/src/ventanaGestionCitas.java
+++ Proyecto_JAVA/src/ventanaGestionCitas.java
@@ -4,72 +4,73 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Vector;
-public class ventanaGestionCitas extends JInternalFrame{
+public class ventanaGestionCitas extends JFrame{
 	private JTable tabla;
-	private Vector<String[]> columnas;
+	private Vector<String> columnas;
 	private Vector<Vector<Object>> datos;
 	private Vector<Object> fila; //Que se queden vacias incluso si no hay datos O ver si hay scrolllbar, entonces que vaya apareciendo secuencialmente
 	private JTextField d, m, y;
-	private JLabel separador; //Simplemente para dejar bonito la seleccion de fecha
 	private JButton aceptar, modificar, borrar, salir; //el primer JButton (Aceptar) es para cambiar la fecha. Mod y Borr gestionan la tabla. Salir cierra el JIF.
+	
 	
 	ventanaGestionCitas() throws ClassNotFoundException, SQLException{
 		super("Gestión de citas existentes");
 		this.setLayout(new BorderLayout(0, 10));
-		PanelFechas(); //Añade panel con las fechas
-		PanelTabla();
 		
+		JPanel pan = PanelFechas();
+		this.add(BorderLayout.NORTH, pan);
+		JTable table = PanelTabla();
+		JScrollPane scroll = new JScrollPane(table);
+		this.add(BorderLayout.CENTER, scroll);
 		
 		
 		this.setVisible(true);
 	}
 	
 	
-	public void PanelFechas () {
+	public JPanel PanelFechas () { //Cambiar y ponerle un JDatePicker
 		JPanel panel = new JPanel();
 		panel.setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
 		d = new JTextField();
 		m = new JTextField();
 		y = new JTextField();
-		separador = new JLabel();
 		aceptar = new JButton("Aceptar");
 		
 		d.setPreferredSize(new Dimension(70,30));
 		m.setPreferredSize(new Dimension(70,30));
 		y.setPreferredSize(new Dimension(70,30));
-		separador.setPreferredSize(new Dimension(40,30));
 		aceptar.setPreferredSize(new Dimension(70,30));
 		aceptar.setActionCommand("introfecha");
 		
 		panel.add(d);
-		panel.add(separador);
 		panel.add(m);
-		panel.add(separador);
 		panel.add(y);
 		panel.add(aceptar);
 		
-		this.add(panel);
+		return panel;
 	}
 	
 	
-	public void PanelTabla() throws SQLException, ClassNotFoundException {
-		JPanel panel = new JPanel();
-		JScrollPane scroll = new JScrollPane();
-		
+	public JTable PanelTabla() throws SQLException, ClassNotFoundException {
 		String[] nomcolumnas = {"ID Cita", "Hora", "Nombre Cliente", "Vehiculo", "Matricula", "Servicio", "Coste"};
 		columnas = new Vector<>();
-		columnas.add(nomcolumnas);
+		for(String col: nomcolumnas) {
+			columnas.add(col);
+		}
+		
 		
 		//No usamos fechas (luego eliminare el panel superior, lo he creado por si acaso) asi que la tabla debe ser dinamica
 		BaseDatos bd = new BaseDatos();
-		ResultSet rs = bd.query("SELECT c.cita_id, c.fecha_hora, cl.nombre, cl.apellido, v.marca, v.modelo, v.matricula, s.nombre, s.costo "
+		ResultSet rs = bd.query("SELECT c.hora, c.cita_id, cl.nombre, cl.apellido, v.marca, v.modelo, v.matricula, s.nombre, s.costo "
 				+ "FROM citas c, clientes cl, vehiculos v, servicios s "
 				+ "WHERE c.cliente_id = cl.cliente_id AND cl.cliente_id = v.id_cliente AND c.servicio_id = s.servicio_id");
-		datos = new Vector<>();
+		String[] horas = {"8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00"};
+		datos = new Vector<>();	
 		while(rs.next()) {
+				
 				fila = new Vector<>();
-				fila.add(rs.getInt(1)); // ID Cita
-				fila.add(rs.getString(2)); //Hora
+				fila.add(rs.getInt(2)); // ID Cita
+				fila.add(rs.getString(1)); //Hora
 				fila.add("" + rs.getString(3) + " " + rs.getString(4)); //Nombre + apellido
 				fila.add("" + rs.getString(5) + " " + rs.getString(6)); //Marca + Modelo
 				fila.add(rs.getString(7)); //Matricula
@@ -79,8 +80,7 @@ public class ventanaGestionCitas extends JInternalFrame{
 		}
 		bd.cerrarConexion();
 		tabla = new JTable(datos, columnas);
-		scroll.add(tabla, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		this.add(scroll);
+		return tabla;
 	}
 	
 	//panelbotones, comprobar funcion tabla, añadir vinculo desde menu a esta ventana y luego ya funciones
