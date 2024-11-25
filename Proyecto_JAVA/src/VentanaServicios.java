ae69281b740e91468bdd2b731fb170ced3513b3c
Juanan <juanan2395@gmail.com>
2024-11-25 15:01:08 +0100

    cambios en servicios, me falta añadir y lo de alex

--- Proyecto_JAVA/src/VentanaServicios.java
+++ Proyecto_JAVA/src/VentanaServicios.java
@@ -4,22 +4,24 @@ import java.sql.*;
 public class VentanaServicios extends JFrame{
 	private JList listaserv;
 	private DefaultListModel<String> modellist;
-	private JButton nuevo, modif, elim;
+	private JButton propiedades, nuevo, borrar;
 	private JLabel [] lbl;
 	private JTextField [] txtfld;
+	private JTextArea desc;
+	private JCheckBox factura;
 	private BaseDatos bd;
 	
 	VentanaServicios() throws SQLException, ClassNotFoundException{
 		super();
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setBounds(10,10,600,600);
-		this.setBackground(Color.LIGHT_GRAY);
-		this.setLayout(new BorderLayout());
+		this.setBounds(10,10,600,300);
+		this.getContentPane().setBackground(Color.GRAY);
+		this.setLayout(new FlowLayout());
 		
 		BaseDatos bd = new BaseDatos();
 		PanelList(bd);
+		PanelBotones(bd);
 		PanelDatos(bd);
-		PanelBotones();
 		this.setVisible(true);
 	}
 	
@@ -39,52 +41,146 @@ public class VentanaServicios extends JFrame{
 			modellist.addElement(rs.getString(1));
 		}
 		listaserv = new JList<>(modellist);
+		listaserv.setPreferredSize(new Dimension(500, 150));
 		listaserv.addListSelectionListener(new escuchadoresServicios(this, bd));
-		JScrollPane scroll = new JScrollPane(listaserv);
+		JScrollPane scroll = new JScrollPane(listaserv, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		
-		PanelLista.add(BorderLayout.CENTER, listaserv);
+		PanelLista.add(BorderLayout.CENTER, scroll);
+		
+		PanelLista.add(BorderLayout.SOUTH, PanelBotones(bd));
 		
 		this.add(BorderLayout.NORTH, PanelLista);
 	}
 	
-	public void PanelDatos (BaseDatos bd) {
+	public JPanel PanelDatos (BaseDatos bd) {
 		JPanel PanelDatos = new JPanel();
 		PanelDatos.setBackground(Color.LIGHT_GRAY);
 		PanelDatos.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
+		PanelDatos.setPreferredSize(new Dimension(600,600));
 		
+		lbl = new JLabel[6];
+		txtfld = new JTextField[6];
 		
-		lbl = new JLabel[5];
-		txtfld = new JTextField[5];
-		
-		String[] labels = {"ID", "Nombre", "Coste", "Fecha Ultima Modificación", "Descripción"};
+		String[] labels = {"ID", "Nombre", "Coste", "Fecha Ultima Modificación", "Descripción", "Generar Factura"};
 		for(int i=0; i<lbl.length; i++) {
 				lbl[i] = new JLabel(labels[i]);
 				lbl[i].setPreferredSize(new Dimension(150,50));
 				PanelDatos.add(lbl[i]);
-				txtfld[i] = new JTextField();
-				txtfld[i].setPreferredSize(new Dimension(300,50));
-				PanelDatos.add(txtfld[i]);
+				if(i < 4 ) {
+					txtfld[i] = new JTextField();
+					txtfld[i].setPreferredSize(new Dimension(300,50));
+					PanelDatos.add(txtfld[i]);
+				}
+				if(i == 5) {
+					desc = new JTextArea();
+					desc.setPreferredSize(new Dimension(300,100));
+					JScrollPane scr = new JScrollPane(desc, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+					PanelDatos.add(scr);
+				}
 			}
-		txtfld[4].setPreferredSize(new Dimension(300,200));
-		this.add(BorderLayout.CENTER, PanelDatos);
+		factura = new JCheckBox();
+		PanelDatos.add(factura);
+		
+		return PanelDatos;
 	}
 	
-	public void PanelBotones () {
+	public JPanel PanelBotones (BaseDatos bd) {
 		JPanel pb = new JPanel();
 		pb.setBackground(Color.LIGHT_GRAY);
 		pb.setLayout(new GridLayout(1, 3, 10, 10));
 		
-		nuevo = new JButton("Añadir");
-		nuevo.setActionCommand("servAdd");
-		modif = new JButton ("Modificar");
-		modif.setActionCommand("servMod");
-		elim = new JButton ("Eliminar");
-		elim.setActionCommand("servElim");
+		propiedades = new JButton("Propiedades");
+		propiedades.setActionCommand("servPropiedades");
+		propiedades.addActionListener(new escuchadoresServicios(this, bd));
+		//propiedades.setEnabled(false);
+		nuevo = new JButton ("Modificar");
+		nuevo.setActionCommand("servMod");
+		nuevo.setEnabled(false);
+		borrar = new JButton ("Eliminar");
+		borrar.setActionCommand("servElim");
+		borrar.setEnabled(false);
 		
+		pb.add(propiedades);
 		pb.add(nuevo);
-		pb.add(modif);
-		pb.add(elim);
-		this.add(BorderLayout.SOUTH, pb);
+		pb.add(borrar);
 		
+		return pb;
 	}
+	
+	
+	public JList getListaserv() {
+		return listaserv;
+	}
+
+	public void setListaserv(JList listaserv) {
+		this.listaserv = listaserv;
+	}
+
+	public DefaultListModel<String> getModellist() {
+		return modellist;
+	}
+
+	public void setModellist(DefaultListModel<String> modellist) {
+		this.modellist = modellist;
+	}
+
+	public JButton getNuevo() {
+		return nuevo;
+	}
+
+	public void setNuevo(JButton nuevo) {
+		this.nuevo = nuevo;
+	}
+
+	public JButton getPropiedades() {
+		return propiedades;
+	}
+
+	public void setPropiedades(JButton propiedades) {
+		this.propiedades = propiedades;
+	}
+
+	public JButton getBorrar() {
+		return borrar;
+	}
+
+	public void setBorrar(JButton borrar) {
+		this.borrar = borrar;
+	}
+
+	public JLabel[] getLbl() {
+		return lbl;
+	}
+
+	public void setLbl(JLabel[] lbl) {
+		this.lbl = lbl;
+	}
+
+	public JTextField[] getTxtfld() {
+		return txtfld;
+	}
+
+	public void setTxtfld(JTextField[] txtfld) {
+		this.txtfld = txtfld;
+	}
+
+	public JCheckBox getFactura() {
+		return factura;
+	}
+
+	public void setFactura(JCheckBox factura) {
+		this.factura = factura;
+	}
+
+	public BaseDatos getBd() {
+		return bd;
+	}
+
+	public void setBd(BaseDatos bd) {
+		this.bd = bd;
+	}
+	
+	
+	
+	
 }
--- Proyecto_JAVA/src/escuchadoresServicios.java
+++ Proyecto_JAVA/src/escuchadoresServicios.java
@@ -1,6 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -16,12 +17,60 @@ public class escuchadoresServicios implements ListSelectionListener, ActionListe
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		switch(e.getActionCommand()) {
+			case "servPropiedades":
+				JOptionPane.showMessageDialog(vs, "HOLA");
+				System.exit(0);
+			break;
+		}	
 	}
 
 	@Override
 	public void valueChanged(ListSelectionEvent e) {
 		// TODO Auto-generated method stub
+		if(!e.getValueIsAdjusting()) { //es true mientra el usuario este haciendo cambios en la seleccion (arrastrando o seleccionando varios o cosas asi)
+			if(vs.getListaserv().getSelectedIndex() != -1) {
+				vs.getPropiedades().setEnabled(true);
+				vs.getNuevo().setEnabled(true);
+				vs.getBorrar().setEnabled(true);
+			}
+			else {
+				vs.getPropiedades().setEnabled(false);
+				vs.getNuevo().setEnabled(false);
+				vs.getBorrar().setEnabled(false);
+			}
+		}
 		
 	}
+	
+	//ENSEÑAR EL PANEL, CARGAR LOS DATOS, CERRAR EL PANEL
+	public void mostrarPanelDatos() { //Nuevo y Propiedades
+		
+	}
+	
+	public void cargarPanelDatos() { //Propiedades
+		
+	}
+	
+	public void cerrarPanelDatos() {
+		
+	}
+	
+	//recoge los datos (si estan todos rellenos!!!) y lo manda a la base de datos. Buscar alguna manera de que tambien refresque el JList
+	public void crearServicio() {
+		
+	}
+	
+	//void o habria que devolver el Jlistselecteditem??. Buscar alguna manera de que tambien refresque el JList
+	public void EliminarServicio() {
+		
+	}
+	
+	//-------------------METODOS ALEX-------------- (añadirle actionlistener al checkbox y luego el if en la parte del actioncommand).
+	public void generarFactura() {
+		
+	}
+	
+	
+	
 }
